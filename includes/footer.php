e93b09ba59b3826de38c7b7578efd14bb3d71815
Aman Verma <amanverma986@gmail.com>
2021-04-27 02:22:49 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 Online Course Registration | By : <a href="https://www.linkedin.com/in/aman-verma-b7051a149/" target="_blank">Aman Verma</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
