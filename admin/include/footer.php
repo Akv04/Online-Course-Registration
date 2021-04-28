ad37af030d33fafc207211903ddb70f616b08b0f
Aman Verma <amanverma986@gmail.com>
2021-04-28 15:09:42 +0530

    Update footer.php

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; 2015 Online Course Registration | By : <a href="http://www.phpgurukul.com/" target="_blank">PHPGURUKUL</a>
+                    &copy; 2021 Online Course Registration | By : <a href="https://www.linkedin.com/in/aman-verma-b7051a149/" target="_blank">Aman Verma</a>
                 </div>
 
             </div>
